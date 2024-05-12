d2e4043df5f35967823a88ed5fab61b5a3814f09
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-05-12 13:13:06 +0530

    12-5-2024

---
+++ Collection/C6_Map.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class C6_Map {
+    public static void main(String[] args) {
+        HashMap hm = new HashMap();
+        Hashtable ht = new Hashtable();
+        LinkedHashMap lhm = new LinkedHashMap();
+        TreeMap tm = new TreeMap();
+
+        hm.put(1, "virat");
+        hm.put(2, "rohit");
+        hm.put(3, "rohan");
+        System.out.println(hm);
+    }
+}
