4bd732042ed606d6f57da357884a0c9514cde00e
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-05-12 11:46:58 +0530

    12-5-2024

---
+++ Collection/C3_PriorityQueue.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class C3_PriorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue pq = new PriorityQueue();
+        pq.add(10);
+        pq.add(20);
+        pq.add(15);
+        pq.add(150);
+        pq.add(75);
+        pq.add(0);
+        System.out.println(pq);
+    }
+}
