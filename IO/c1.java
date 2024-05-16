88ab6573b17bee484d115a6fa22d90b18d168aad
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-05-16 20:54:18 +0530

    16-5-2024

--- IO/c1.java
+++ IO/c1.java
@@ -1,1 +1,10 @@
-import java.io.*
+import java.io.*;
+
+public class c1 {
+    public static void main(String[] args) throws IOException {
+        File f1 = new File("pw.txt");
+        System.out.println(f1.exists());
+        f1.createNewFile();
+        System.out.println(f1.exists());
+    }
+}
