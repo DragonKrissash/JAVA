91c0533850e6d402ce3165ed90ddb1896e67426d
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-05-17 16:23:11 +0530

    17-5-2024

---
+++ IO/c6.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class c6 {
+    public static void main(String[] args) throws IOException {
+        File f = new File("PW/pw.txt");
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        String line = br.readLine();
+        while (line != null) {
+            System.out.println(line);
+            line = br.readLine();
+        }
+    }
+}
