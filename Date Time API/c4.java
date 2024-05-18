d11bbb64f584ec6f5d3b7e7aea212f1068e38ffc
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-05-18 08:34:00 +0530

    18-5-1014

--- Date Time API/c4.java
+++ Date Time API/c4.java
@@ -4,7 +4,14 @@ import java.util.stream.*;
 public class c4 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-        Stream<Integer> streamData = list.stream();
-        streamData.forEach(n -> System.out.println(n));
+        // Stream<Integer> streamData = list.stream();
+        // Stream<Integer> evenData = streamData.filter(n -> n % 2 == 0);
+        // Stream<Integer> sortedData = evenData.sorted();
+
+        Stream<Integer> mapData = list.stream()
+                .filter(n -> n % 2 == 0)
+                .sorted()
+                .map(n -> 2 * n);
+        mapData.forEach(n -> System.out.println(n));
     }
 }
